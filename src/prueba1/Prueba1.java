6f11deaef2cc426a0ac30bc45eca29e723300a44
diego's <diego's@diegos>
2018-10-10 12:42:14 -0500

    Version 0.2

--- src/prueba1/Prueba1.java
+++ src/prueba1/Prueba1.java
@@ -17,6 +17,8 @@ public class Prueba1 {
     public static void main(String[] args) {
         System.out.println("Prueba 1 ");
         System.out.println("Prueba 2 ");
+        System.out.println("Prueba 3 ");
+        System.out.println("Prueba 4 ");
     }
     
 }
