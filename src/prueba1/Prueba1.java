d20508551ed7dd8f2778520f4ff171b1c2b4be0f
diegodjcv2394 <40705193+diegodjcv2394@users.noreply.github.com>
2018-10-10 13:07:10 -0500

    Version 0.2

--- src/prueba1/Prueba1.java
+++ src/prueba1/Prueba1.java
@@ -21,8 +21,6 @@ public class Prueba1 {
         System.out.println("Prueba 4 ");
         System.out.println("Prueba 1 ");
         System.out.println("Prueba 2 ");
-        System.out.println("Prueba 3 ");
-        System.out.println("Prueba 4 ");
     }
     
 }
